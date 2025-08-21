e5566f77bf6e1ad28dc2210a76bdf737d13fb0a2
Tuan Nguyen <2124802010170@student.tdmu.edu.vn>
2025-08-21 14:59:07 +0700

    feat(admin): setup admin area and routing

---
+++ InvestDapp/Areas/admin/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InvestDapp.Areas.admin.Controllers
+{
+    [Area("Admin")]  // Khai báo Area
+    [Route("admin/[controller]/[action]")]  
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ InvestDapp/Areas/admin/Controllers/Manage_CampaignsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InvestDapp.Areas.admin.Controllers
+{
+    public class Manage_CampaignsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
